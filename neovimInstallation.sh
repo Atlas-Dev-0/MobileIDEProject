b5bb332fecde30e7fdf626a094b1342113501a71
Atlas-Dev-0 <kenneth.gonzales@bicol-u.edu.ph>
2024-06-27 10:59:31 +0800

    wip

--- neovimInstallation.sh
+++ neovimInstallation.sh
@@ -5,9 +5,10 @@ echo "| .  . || |_/ / |  \/"
 echo "| |\/| ||    /| | __ "
 echo "| |  | || |\ \| |_\ \/"
 echo "\_|  |_/\_| \_|\____/"
-
+echo ""
 echo "MobileIDEProject"
-
+echo ""
+echo ""
 echo "Installing APT package manager.."
  
 # Function to check the exit status of a command
@@ -30,17 +31,29 @@ check_command apt update -y
 check_command apt upgrade -y
 
 echo "Apt installed and is updated "
-
+echo ""
 echo "Installing Neovim..."
-
+echo ""
 check_command apt install neovim -y
 
 echo "Neovim is installed..."
-
+echo ""
+echo ""
+echo "Installing essential build tools (c compilers) "
+check_command apt install build-essential -y
+echo ""
+echo ""
+echo "Installing python3"
+echo ""
+check_command apt install python3 -y
+echo ""
+echo ""
+echo ""
 echo "Configuring Neovim..."
 cd ~
 cd ~/.config/
 check_command git clone --branch MobileIDEProject https://github.com/Atlas-Dev-0/NVDF.git nvim 
 
+echo "Neovim Configured...You may open it now"
 
 
