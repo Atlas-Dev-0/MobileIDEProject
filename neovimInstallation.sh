7a4929e49444a3d3ee3970f3e0e22f29bba389d8
Atlas-Dev-0 <kenneth.gonzales@bicol-u.edu.ph>
2024-06-27 10:28:16 +0800

    wip

---
+++ neovimInstallation.sh
@@ -0,0 +1,31 @@
+
+echo "___  _________ _____" 
+echo "|  \/  || ___ \  __ \/"
+echo "| .  . || |_/ / |  \/"
+echo "| |\/| ||    /| | __ "
+echo "| |  | || |\ \| |_\ \/"
+echo "\_|  |_/\_| \_|\____/"
+
+echo "MobileIDEProject"
+echo "Installing Neovim..."
+
+cd ~
+cd ~/.config/
+git clone --branch MobileIDEProject https://github.com/Atlas-Dev-0/NVDF.git
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
