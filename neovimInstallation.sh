f5eec1717d8b4c3b336028d5b4675361ebe384c4
Atlas-Dev-0 <kenneth.gonzales@bicol-u.edu.ph>
2024-06-27 10:40:04 +0800

    wip

--- neovimInstallation.sh
+++ neovimInstallation.sh
@@ -7,25 +7,40 @@ echo "| |  | || |\ \| |_\ \/"
 echo "\_|  |_/\_| \_|\____/"
 
 echo "MobileIDEProject"
+
+echo "Installing APT package manager.."
+ 
+# Function to check the exit status of a command
+check_command() {
+	"$@"
+	status=$?
+	if [ $status -ne 0 ]; then
+        echo "Error with $1" >&2
+        exit $status
+    fi
+}
+
+# Install the pkg package
+check_command pkg install apt -y
+
+# Update the package list
+check_command apt update -y
+
+# Upgrade all packages
+check_command apt upgrade -y
+
+echo "Apt installed and is updated "
+
 echo "Installing Neovim..."
 
+check_command apt install neovim -y
+
+echo "Neovim is installed..."
+
+echo "Configuring Neovim..."
 cd ~
 cd ~/.config/
-git clone --branch MobileIDEProject https://github.com/Atlas-Dev-0/NVDF.git
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+check_command git clone --branch MobileIDEProject https://github.com/Atlas-Dev-0/NVDF.git nvim -y
 
 
 
